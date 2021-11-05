a6a87e3fc9b24ff4497592f27237997ff216d009
Valeria Insogna <valinsogna@gmail.com>
2021-11-05 16:26:56 +0100

    var

--- c++/04_custom_types/01_enum.cpp
+++ c++/04_custom_types/01_enum.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-
+//Enum defines const expressions with different values compolsury
 enum color { red, yellow, green };
 // enum color{red=0, yellow=1, green=2}; // equivalent
-
+//But what if I define:
+//enum t {blue, red, green} -> This name clashing has been resolved with scope enum in C++11
 void dwim(const color c) {
+  //SWITCH works only with enum, char, (unsigned/short)int. Not with strings or double!
   switch (c) {
     case red:
       std::cout << "option 1: red\n";
-      break;
+      break;//exit from the inner scope/loop
     case yellow:
       std::cout << "option 2: yellow\n";
       break;
@@ -21,7 +23,7 @@ void dwim(const color c) {
 }
 
 int main() {
-  color opt{red};
+  color opt{red};//named color variable of typew color
   // opt = 3; // cannot assign int to enum
   int a{opt};  // but they implicitly convert to integers
 
