9e76d9c422af652b8ef3ef673fad7afce70821f1
Valeria Insogna <valinsogna@gmail.com>
2021-11-11 23:33:18 +0100

    varie

--- c++/05_copy_move_semantics/01_surprise.cpp
+++ c++/05_copy_move_semantics/01_surprise.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
-
-struct X {
-  char member;
+#include <utility>
+struct X { //no constructor nor destructor
+  char member; //built in type
 };
 
 int main() {
-  X x1;                 // default ctor
+  X x1;                 // default ctor: uninitializer
   X x2{x1};             // copy ctor
-  X x3{std::move(x2)};  // move ctor
+  X x3{std::move(x2)};  // move ctor: ++11
   X x4{};               // default ctor calling {} to each member
   x4 = x3;              // copy assignment
   X x5;                 // default ctor
   x5 = std::move(x4);   // move assignment
-  std::cout << x5.member << std::endl;  // what do you expect??
+  std::cout << x5.member << std::endl;  // what do you expect?? Whatever
+  //But if I choose 'X x1{}' for line 8, it returns empty char
   return 0;
 }
+//These code compiles beacuse if I don't specify a custom constructor,
+// the compiler syntetize a default ctor, and the same for all move/copy ..
+//If I define the constructor insted there is no difference between X x1 and X x1{}
