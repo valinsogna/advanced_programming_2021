73db03a2f2860949bfb2b26bd2e73957aa4ba9ea
Valeria Insogna <valinsogna@gmail.com>
2021-11-22 14:37:46 +0100

    varie

--- c++/08_inheritance/02_virtual.cpp
+++ c++/08_inheritance/02_virtual.cpp
@@ -10,7 +10,9 @@ struct Animal {
   }
 
   Animal() : Animal{0, 0} {}  // delegating constructor
-
+  //DINAMIC BINDING: virtual on the function that can be overwritten
+  //It is different than function overloading!
+  //It is function overwriting: same func, same arg, different classes!
   virtual void speak() const = 0;  // pure virtual function ==> abstract class
   virtual void info() const noexcept {
     std::cout << "age:\t" << age << '\n' << "weight:\t" << weight << '\n';
@@ -20,15 +22,18 @@ struct Animal {
 };
 
 struct Dog : public Animal {
-  void speak() const noexcept override { std::cout << "Bau\n"; }
+  //override (from C++11) double checks if it's virtual in the base class
+  //otherwise prima c'era anche qui virtual ma poi era poco chiaro
+  void speak() const noexcept override { std::cout << "Bau\n"; } 
   Dog() = default;
   Dog(const unsigned int a, const double d) : Animal{a, d} {}
 };
 
 struct Snake : public Animal {
   bool dangerous;
+  //Ctor first generates the parent and then add the things for the child
   Snake(const unsigned int a, const double w, const bool b)
-      : Animal{a, w}, dangerous{b} {}
+      : Animal{a, w}, dangerous{b} {} 
   explicit Snake(const bool b) : Animal{}, dangerous{b} {}
   void info() const noexcept override {
     Animal::info();
