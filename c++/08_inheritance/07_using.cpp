c5528c68151ebd6d051b25652c3bde867f6af2d2
Valeria Insogna <valinsogna@gmail.com>
2021-11-22 15:15:15 +0100

    varie

--- c++/08_inheritance/07_using.cpp
+++ c++/08_inheritance/07_using.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
-
+//I do the overload of a function inside a ?
 struct foo {
   void f(int x) { std::cout << "int " << x << std::endl; }
 };
 
 struct bar : public foo {
-  using foo::f;  // brings all foo::f to this scope
+  //using foo::f;  // brings all foo::f to this scope
   void f(char x) { std::cout << "char " << x << std::endl; }
 };
 
 int main() {
   bar b;
-  b.f(33);
+  b.f(33);//doesn't call func at line 4! Function overloading is not resolved across the hierarchy!
+  //with line 8 yes!
 }
