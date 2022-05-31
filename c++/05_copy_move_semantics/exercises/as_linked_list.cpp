236913fda2abd765571733122b109bfa051eb20b
Valeria Insogna <valinsogna@gmail.com>
2022-05-31 18:08:23 +0200

    less 8 review

--- c++/05_copy_move_semantics/exercises/as_linked_list.cpp
+++ c++/05_copy_move_semantics/exercises/as_linked_list.cpp
@@ -1,7 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
-#include <memory>
+#include <memory> //unique_ptr
 
 enum class method { push_back, push_front };
 
@@ -16,15 +16,15 @@ class _iterator {
   using difference_type = std::ptrdiff_t;
   using iterator_category = std::forward_iterator_tag;
 
-  explicit _iterator(node* x) : current{x} {}
+  explicit _iterator(node* x) : current{x} {} //ctor
   reference operator*() const { return current->value; }
-  _iterator& operator++() {  // pre-increment
+  _iterator& operator++() {  // pre-increment ++i
     current = current->next.get();
     return *this;
   }
-  _iterator operator++(int) {  // post-increment
+  _iterator operator++(int) {  // post-increment i++
     auto tmp = *this;
-    ++(*this);
+    ++(*this); // i++ is slower than ++i since it first return the copy and then increment it!
     return tmp;
   }
   friend bool operator==(const _iterator& x, const _iterator& y) {
@@ -32,53 +32,62 @@ class _iterator {
   }
 
   friend bool operator!=(const _iterator& x, const _iterator& y) {
-    return !(x == y);
+    return !(x == y); // We can call the operator== since it is friend instead of:
+    // x.current != y.current;
   }
 };
 
 template <typename T>
 class List {
-  struct node {
+  struct node { // Useful nested since the template is the same!
     T value;
-    std::unique_ptr<node> next;
-    node(const T& x, node* p)
+    std::unique_ptr<node> next; // l-value ctor
+    node(const T& x, node* p) // ctor
         : value{x},  // copy ctor
           next{p} {}
 
-    node(T&& x, node* p)
+    node(T&& x, node* p) // r-value ctor
         : value{std::move(x)},  // move ctor
           next{p} {}
-
+    // a custom ctor
     explicit node(const std::unique_ptr<node>& p) : value{p->value} {
       if (p->next)
         next = std::make_unique<node>(p->next);
     }
   };
-  std::unique_ptr<node> head;
+  std::unique_ptr<node> head; // at the begin of the list
 
  public:
   using iterator = _iterator<node, T>;
   using const_iterator = _iterator<node, const T>;
 
-  void insert(const T& x, method m) { _insert(x, m); }
+  //With move semantics in C++11 is not a bad idea to pass by REF
+  // Avoid code duplication:
+  void insert(const T& x, method m) { _insert(x, m); } 
   void insert(T&& x, method m) { _insert(std::move(x), m); }
 
-  List() = default;
-  List(List&&) = default;
-  List& operator=(List&&) = default;
+  List() = default; // Fine since there are NO raw ptr so everything is initialzed
+  List(List&&) = default; // same
+  List& operator=(List&&) = default; // move assign (no raw ptr)
 
-  List(const List& that) {
-    if (that.head)
-      head = std::make_unique<node>(that.head);
+  List(const List& that) { // copy ctor
+  // Not default one since the default deletes the list because
+  // it will invoke a copy ctor at each memeber but the ptr is unique -> error
+    if (that.head) // I am in public BUT I can access private data of the same object of my type!
+      head = std::make_unique<node>(that.head); // it calls the ctor of node explicit node
   }
 
-  List& operator=(const List& x) {
+  List& operator=(const List& x) { //copy assign
     head.reset();
     auto tmp = x;
     (*this) = std::move(tmp);
     return *this;
   }
 
+  // Range-based for loop
+  // We can't use raw ptr otherwise when ++ptr we won't go to the next node,
+  // but to the next address. (const T* begin() const)
+  // Iterator is an augmented ptr! Like a generalization of a ptr!
   auto begin() { return iterator{head.get()}; }
   auto end() { return iterator{nullptr}; }
 
@@ -94,11 +103,31 @@ class List {
   }
 
  private:
+  // X is != T
+  // When X is deduced and when T?
+  // T has been already deduced when calling in main 
+  // l.insert(3,method::push_front) since insert() is invoked.
+  // When _insert() is invoked instead, X will be deduced by the args passed to the function
   template <typename X>
-  void _insert(X&& x, method m) {  // forwarding ref.
+  void _insert(X&& x, method m) {  // forwarding ref. (!= std::move) can accept r and l value ref!
+  // Check if the list is empty
+  /*
+  To check if a `std::unique_ptr<T> p` is different from `nullptr`
+  you can simply use `if(p)` (there is no need to write `if (p.get())`)
+  */
     if (!head) {                   // head == nullptr
+      // head.reset(new node{x,nullptr});
+      // Recall:
+      // void reset(T* ptr=nullptr)` deletes `current_ptr` (if any) and set `current_ptr = ptr`.
+      
+      // BUT BETTER USE make_unique!
       head = std::make_unique<node>(std::forward<X>(x), nullptr);
-      return;
+      // func make_unique takes as arg the constructor of the object!
+      // and returns a unique_ptr to the newly created object.
+      // You can pass no args => deafult ctor
+      // Or custom ctors!
+      
+      return; // exit from the function
     }
     switch (m) {
       case method::push_back:
@@ -113,10 +142,11 @@ class List {
     };
   }
 
-  node* last_node() {
+  node* last_node() { 
     auto tmp = head.get();
+    //since we don't know how long is the list we perfrom a while loop (not a for)
     while (tmp->next)  // tmp->next != nullptr
-      tmp = tmp->next.get();
+      tmp = tmp->next.get(); // we can safely access tmp next since in insert() we already checked it
     return tmp;
   }
 
@@ -129,19 +159,38 @@ class List {
   }
 
   void push_front(const T& x) {
-    // auto tmp = new node{x,head.release()};
-    // head.reset(tmp);
+    // I must change the head ptr to the new pushed node!
+    // / not .get() otherwise 2 ptr pointing same node! NO uniqueness!
+    // So use .release()!
+    /* Recall:
+      - `T* release()` Releases the ownership of the managed object 
+        if any. `get()` returns `nullptr` after the call.
+      - `void reset(T* ptr=nullptr)` Delete `current_ptr` (if any) and set `current_ptr = ptr`.
+    */
 
+    // auto tmp = new node{x,head.release()}; // tmp is a RAW ptr, not a smart one!
+    // head.reset(tmp); // must delete it since head is gonna be another ptr to the new head!
+
+    // These 2 lines csn be rewritten in 1 short:
     // head.reset(new node{x,head.release()});
 
+    // OR using make_unique:
     head = std::make_unique<node>(x, head.release());
 
     // auto tmp = std::make_unique<node>(x,head.release());
-    // head.swap(std::make_unique<node>(x,head.release()));
+    // head.swap(std::make_unique<node>(x,head.release())); //move swap same things!
   }
-  void push_front(T&& x) {
-    // head = std::make_unique<node>(x,head.release()); // l-val
-    head = std::make_unique<node>(std::move(x), head.release());  // r-val
+  void push_front(T&& x) { //R-value 
+    // head = std::make_unique<node>(x,head.release()); // calls l-val ctor: x has a name!
+    // R-value ref are passed byt other func as l-values since they have a name!
+    head = std::make_unique<node>(std::move(x), head.release());  // calls r-val ctor
+    // Why not simply x but std::move(x)? We wanted the ctor like that!
+    /*
+    Recall:
+    It changes an expression from being an l-value (such as a named variable)
+     to being an r-value.
+    You're allowing the compiler to cannibalize x.
+    */
   }
 };
 
